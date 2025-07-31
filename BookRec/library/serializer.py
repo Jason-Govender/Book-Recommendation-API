59eb704288b66614529ba9bdc30e1e4e4106d9c3
Jason Govender <jasongovend@gmail.com>
2025-07-31 12:20:17 +0200

    Added serializer for ratings data.

--- BookRec/library/serializer.py
+++ BookRec/library/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Book
 from .models import Book_Meta
+from .models import Rating_Data
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
@@ -9,4 +10,9 @@ class BookSerializer(serializers.ModelSerializer):
 class BookMetaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book_Meta
+        fields = '__all__'
+
+class RatingDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating_Data
         fields = '__all__'
