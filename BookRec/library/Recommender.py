33834cc5e7d9bd78e066ea6bf1194112b823f686
Jason Govender <jasongovend@gmail.com>
2025-07-31 14:04:50 +0200

    Created all API views.

--- BookRec/library/Recommender.py
+++ BookRec/library/Recommender.py
@@ -3,7 +3,7 @@ import json
 import os
 import random
 import pandas as pd
-from serializer import BookMetaSerializer
+from .serializer import BookMetaSerializer
 from .models import Rating_Data, Book_Meta
 from rest_framework.response import Response
 
@@ -15,20 +15,20 @@ scaler = joblib.load(os.path.join(MODEL_DIR, "scaler.pkl"))
 with open(os.path.join(MODEL_DIR, "cluster_book_map.json")) as f:
     top_books = json.load(f)
 
-def find_cluster(request):
-    ratings = Rating_Data.objects.filter(user_id=int(request.session["user_id"]))
+def find_cluster():
+    ratings = Rating_Data.objects.all()
     ratings_df = pd.DataFrame(list(ratings.values()))
     user_item_matrix = ratings_df.pivot_table(index="user_id", columns="book_id", values="rating").fillna(0)
     scaled_ratings = scaler(user_item_matrix)
     cluster = model.predict(scaled_ratings)[0]
     return cluster
 
-def recommend(request):
-    cluster = find_cluster(request)
-    choice = random.choice(top_books.get(cluster))
-    book = Book_Meta.objects.filter(book_id=int(choice.book_id)).first()
-    serializer = BookMetaSerializer(book)
-    return Response(serializer.data)
+def recommend():
+    cluster = find_cluster()
+    books_in_cluster = top_books.get(cluster, [])
+    choices = random.sample(books_in_cluster, min(len(books_in_cluster), 5))
+    books = Book_Meta.objects.filter(book_id=int(choices.book_id)).first()
+    return books
 
 
 
--- BookRec/library/views.py
+++ BookRec/library/views.py
@@ -1,20 +1,31 @@
-from rest_framework import generics
-from .models import Book
-from .serializer import BookSerializer
+from rest_framework import viewsets, filters
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from Recommender import recommend
+from .models import Book, Book_Meta, Rating_Data
+from .serializer import BookSerializer, BookMetaSerializer, RatingDataSerializer
 
-class BookListCreateView(generics.ListCreateAPIView):
-    serializer_class = BookSerializer
-
-    def get_queryset(self):
-        queryset = Book.objects.all()
-        author = self.request.query_params.get('author')
-        title = self.request.query_params.get('title')
-        if author:
-            queryset = queryset.filter(author__icontains=author)
-        if title:
-            queryset = queryset.filter(title__icontains=title)
-        return queryset
-
-class BookRetrieveDestroyView(generics.RetrieveDestroyAPIView):
+class BookList(viewsets.modelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class MetaList(viewsets.modelViewSet):
+    queryset = Book_Meta.objects.all()
+    serializer_class = BookMetaSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author']
+
+class RatingList(viewsets.modelViewSet):
+    queryset = Rating_Data.objects.all()
+    serializer_class = RatingDataSerializer
+
+class RecommendView(APIView):
+    def get(self, request):
+        recommendation = recommend()
+        serializer = BookMetaSerializer(recommendation)
+        return Response(serializer.data)
+
+
+
+
+
